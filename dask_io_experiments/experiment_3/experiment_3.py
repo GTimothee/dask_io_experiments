833492dc3e4b1ed205af6d63d3d457e51c93e92e
GTimothee <ava_punksmash@protonmail.com>
2020-05-01 23:57:24 -0400

    results verified with dask.array.allclose

--- dask_io_experiments/experiment_3/experiment_3.py
+++ dask_io_experiments/experiment_3/experiment_3.py
@@ -25,6 +25,7 @@ from dask_io.optimizer.utils.get_arrays import get_dask_array_from_hdf5
 from dask_io.optimizer.utils.array_utils import inspect_h5py_file
 from dask_io.optimizer.utils.get_arrays import create_random_dask_array, save_to_hdf5
 from dask_io.optimizer.cases.resplit_case import compute_zones
+from dask_io.optimizer.cases.resplit_utils import get_blocks_shape, get_named_volumes, convert_Volume_to_slices
 
 def create_test_array_nochunk(file_path, shape):
     """ Create input dask array for the experiment with no physical chunks.
@@ -56,22 +57,24 @@ if __name__ == "__main__":
 
     # for resplit
     O = (1,40,40)
-    I = (1,60,60)
+    I = (1,30,30)
 
+    # split input data into input files
     tmpdir = tempfile.TemporaryDirectory()
     os.chdir(tmpdir.name)
     create_test_array_nochunk(inputfilepath, inputfileshape)
     split(inputfilepath, I)
+
+    # create reconstructed array from input files
     case = Merge('./reconstructed.hdf5') # dont care about the name of outfile bec we retrieve without actually merging
     case.merge_hdf5_multiple('./', store=False)
     reconstructed_array = case.get()
     print(reconstructed_array)
+    reconstructed_array = reconstructed_array.rechunk(1,60,60)
 
-    
+    # creations of data for dask store function
     d_arrays, d_regions = compute_zones((1,60,60), (1,40,40), (1,120, 120), [1])
-
     out_files = list() # to keep outfiles open during processing
-
     sources = list()
     targets = list()
     regions = list()
@@ -96,18 +99,40 @@ if __name__ == "__main__":
             regions.append(reg)
 
 
+    # storage: creation of task graph
     task = da.store(sources, targets, regions=regions, compute=False)
 
+    # computation
     with Profiler() as prof, ResourceProfiler(dt=0.25) as rprof, CacheProfiler() as cprof:
         with dask.config.set(scheduler='single-threaded'):
             task.compute()
-        visualize([prof, rprof, cprof])
+        # visualize([prof, rprof, cprof])
 
+    # sanity check
     outfiles = list()
     for fpath in glob.glob("[0-9].hdf5"):  # remove split files from previous tests
-        f = h5py.File(fpath, 'r')
         print(f'Filename: {fpath}')
-        inspect_h5py_file(f)
+        with h5py.File(fpath, 'r') as f:
+            inspect_h5py_file(f)
 
+    # prepare ground truth for verification
+    arrays_expected = dict()
+    outfiles_partititon = get_blocks_shape((1,120, 120), O)
+    outfiles_volumes = get_named_volumes(outfiles_partititon, O)
+    for outfilekey, volume in outfiles_volumes.items():
+        slices = convert_Volume_to_slices(volume)
+        arrays_expected[outfilekey] = reconstructed_array[slices[0], slices[1], slices[2]]
 
-    task.visualize(optimize_graph=False)
+    # verify
+    for fpath in glob.glob("[0-9].hdf5"):   
+        outputfile_index = int(fpath.split('.')[0])
+        print(f'Output file index: ', outputfile_index)
+
+        array_stored = get_dask_array_from_hdf5(fpath, '/data', logic_cs="dataset_shape")
+        arr_expected = arrays_expected[outputfile_index]
+        print("equal:", da.allclose(array_stored, arr_expected).compute())
+        print("stored:", array_stored[slice(0, 1, None),slice(0, 1, None),slice(0,10,None)].compute())
+        print("expected", arr_expected[slice(0, 1, None),slice(0, 1, None),slice(0,10,None)].compute())
+        
+
+    # task.visualize(optimize_graph=False)
